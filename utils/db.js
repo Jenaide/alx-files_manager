6702e8727c2bd072ad41a105ba65720462aba0c1
Jenaide <jenaidesibolie@gmail.com>
2023-10-02 12:08:51 -0700

    bug fix in db

--- utils/db.js
+++ utils/db.js
@@ -1,40 +1,35 @@
-const { MongoClient } = require('mongodb');
+import mongodb from 'mongodb';
+import Collection from 'mongodb/lib/collection';
 
 
 class DBClient {
-    /**
-     * class that creates a DBClient instance
-     */
-    constructor() {
-        const host = process.env.DB_HOST || 'localhost';
-        const port = process.env.DB_PORT || 27017;
-        const database = process.env.DB_DATABASE || 'files_manager';
-        const url = `mongodb://${host}:${port}/${database}`;
+  /**
+   * class that creates a DBClient instance
+   */
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    const database = process.env.DB_DATABASE || 'files_manager';
+    const url = `mongodb://${host}:${port}/${database}`;
 
-        this.client = new MongoClient(url, { useUnifiedTopology: true });
-        this.client.connect();
+    this.client = new MongoClient(url, { useUnifiedTopology: true });
+    this.client.connect();
     }
 
-    async isAlive() {
-        try {
-            await this.client.connect();
-            return true;
-        } catch (err) {
-            return false;
-        }
-    }
+  isAlive() {
+    return this.client.connect();
+  }
 
-    async nbUsers() {
-        const userCollection = await this.client.db().collection('users');
-        return userCollection.countDocuments();
-    }
+  async nbUsers() {
+    const userCollection = await this.client.db().collection('users');
+    return userCollection.countDocuments();
+  }
 
-    async nbFiles() {
-        const fileCollection = await this.client.db().collection('files');
-        return fileCollection.countDocuments();
-    }
+  async nbFiles() {
+    const fileCollection = await this.client.db().collection('files');
+    return fileCollection.countDocuments();
+  }
 }
 
-const dbClient = new DBClient();
-
-module.exports = { DBClient, dbClient };
+export const dbClient = new DBClient();
+export default dbClient;
