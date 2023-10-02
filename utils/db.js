b4ef4e4d2f0dba16f039e1055c341549349fe016
Jenaide <jenaidesibolie@gmail.com>
2023-10-02 20:39:36 +0200

    'db'

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+import mongodb from 'mongodb';
+
+
+class DBClient {
+    /**
+     * class that creates a DBClient instance
+     */
+    constructor() {
+        const host = process.env.DB_HOST || 'localhost';
+        const port = process.env.DB_PORT || 27017;
+        const database = process.env.DB_DATABASE || 'files_manager';
+        const url = `mongodb://${host}:${port}/${database}`;
+
+        this.client = new MongoClient(url, { useUnifiedTopology: true });
+        this.client.connect();
+    }
+
+    async isAlive() {
+        try {
+            await this.client.connect();
+            return true;
+        } catch (err) {
+            return false;
+        }
+    }
+
+    async nbUsers() {
+        const userCollection = await this.client.db().collection('users');
+        return userCollection.countDocuments();
+    }
+
+    async nbFiles() {
+        const fileCollection = await this.client.db().collection('files');
+        return fileCollection.countDocuments();
+    }
+}
+
+const dbClient = new DBClient();
+
+module.exports = { DBClient, dbClient };
